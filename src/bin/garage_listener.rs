8cf8b4581c9bc5f5277c19ddeaffceee34b2fce7
Andy <andrew.dunn@ajja.org.uk>
2023-01-06 14:02:15 +0000

    refactor(garage_listener): Use iter::repeat_with
    As I prefer the use of iterators over loop statements, this commit
    changes the structure of `garage_listener.rs` to use
    `iter::repeat_with`, rather than nested `loop` statements.

--- src/bin/garage_listener.rs
+++ src/bin/garage_listener.rs
@@ -1,5 +1,5 @@
 use std::{
-    io,
+    io, iter,
     net::{Ipv4Addr, UdpSocket},
     time::Duration,
 };
@@ -8,30 +8,32 @@ pub fn main() -> io::Result<()> {
     static IP_MULTI: Ipv4Addr = Ipv4Addr::new(224, 192, 32, 29);
     const TIMEOUT: Duration = Duration::from_secs(2);
 
-    let mut buff = [0; 256];
     let listener = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 22600))?;
 
     listener.join_multicast_v4(&IP_MULTI, &Ipv4Addr::UNSPECIFIED)?;
     listener.set_read_timeout(Some(TIMEOUT))?;
 
-    loop {
-        let (size, addr) = loop {
-            let (size, addr) = listener.recv_from(&mut buff)?;
+    iter::repeat_with(|| {
+        let mut buff = [0; 256];
 
-            match (size, buff) {
-                (3, [71, 61, _x @ 48..=51, ..]) => break (size, addr), // Read expected data
-                (0, _) => return Ok(()),                               // Socket has closed
-                _ => continue,                                         // Unexpected data
-            }
-        };
+        listener
+            .recv_from(&mut buff)
+            .map(|(size, addr)| (size, addr, buff))
+    })
+    .take_while(|x| {
+        x.as_ref()
+            .map_or(true, |y| matches!(y, (3, _, [71, 61, _x @ 48..=51, ..])))
+    })
+    .try_for_each(|x| {
+        x.map(|(size, addr, buff)| {
+            let buff = String::from_utf8_lossy(&buff[..size]);
 
-        let buff = String::from_utf8_lossy(&buff[..size]);
-
-        println!(
-            "\x1B[2J\x1B[1;1H\
-            Size: {size:?}\n\
-            Contents: {buff:?}\n\
-            Address: {addr:?}"
-        );
-    }
+            println!(
+                "\x1B[2J\x1B[1;1H\
+                Size: {size:?}\n\
+                Contents: {buff:?}\n\
+                Address: {addr:?}"
+            );
+        })
+    })
 }
