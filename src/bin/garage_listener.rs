b186a0e63bb0e4f3119f7eb924c4f16a0973aa6c
Andy <andrew.dunn@ajja.org.uk>
2023-01-07 02:32:05 +0000

    refactor(garage_listener): Switch to iter::from_fn
    This commit updates `garage_listener.rs`, to use `iter::fn_from`, rather
    than `iter::repeat_with` and `Iterator::take_while`.

--- src/bin/garage_listener.rs
+++ src/bin/garage_listener.rs
@@ -8,19 +8,18 @@ pub fn main() -> io::Result<()> {
     static IP_MULTI: Ipv4Addr = Ipv4Addr::new(224, 192, 32, 29);
     const TIMEOUT: Duration = Duration::from_secs(2);
 
-    let listener = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 22600))?;
+    let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 22600))?;
 
-    listener.join_multicast_v4(&IP_MULTI, &Ipv4Addr::UNSPECIFIED)?;
-    listener.set_read_timeout(Some(TIMEOUT))?;
+    sock.join_multicast_v4(&IP_MULTI, &Ipv4Addr::UNSPECIFIED)?;
+    sock.set_read_timeout(Some(TIMEOUT))?;
 
-    iter::repeat_with(|| {
+    iter::from_fn(|| {
         let mut buff = [0; 256];
 
-        listener
-            .recv_from(&mut buff)
-            .map(|(size, addr)| (size, addr, buff))
+        sock.recv_from(&mut buff)
+            .map(|(size, addr)| size.ne(&0).then_some((size, addr, buff)))
+            .transpose()
     })
-    .take_while(|x| !matches!(x, Ok((0, ..))))
     .filter(|x| matches!(x, Err(_) | Ok((3, _, [71, 61, 48..=51, ..]))))
     .try_for_each(|x| {
         x.map(|(size, addr, buff)| {
