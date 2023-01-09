32d93ea408e8e8bfac406abc653926bac4664f31
Andy <andrew.dunn@ajja.org.uk>
2023-01-09 02:52:03 +0000

    feat(local_listener): Wait on cURL before exit
    Ensures `local_listener.rs` waits for `cURL` to exit, before the program
    exits.

--- src/bin/local_listener.rs
+++ src/bin/local_listener.rs
@@ -8,7 +8,7 @@ pub fn main() -> io::Result<()> {
     let mut buff = [0; 1024];
     let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 7878))?;
 
-    Command::new("curl")
+    let mut child = Command::new("curl")
         .arg("http://127.0.0.1:7878")
         .arg("-i")
         .spawn()?;
@@ -29,5 +29,7 @@ pub fn main() -> io::Result<()> {
         Content-Length: 0\r\n\r\n";
 
     stream.write(RESPONSE)?;
-    stream.flush()
+    stream.flush()?;
+
+    child.wait().map(drop)
 }
