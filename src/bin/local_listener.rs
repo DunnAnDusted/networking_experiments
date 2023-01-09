ad5f0fb2d100cca7a38a0a250496931b79cbb4a9
Andy <andrew.dunn@ajja.org.uk>
2023-01-09 03:00:44 +0000

    docs(local_listener): Add explanetory comments

--- src/bin/local_listener.rs
+++ src/bin/local_listener.rs
@@ -8,6 +8,8 @@ pub fn main() -> io::Result<()> {
     let mut buff = [0; 1024];
     let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 7878))?;
 
+    // Runs `cURL` as a  child process, to ensure consistent arguments,
+    // by avoiding the need to execute it manually from another shell tab.
     let mut child = Command::new("curl")
         .arg("http://127.0.0.1:7878")
         .arg("-i")
@@ -24,6 +26,9 @@ pub fn main() -> io::Result<()> {
         Contents: {buff:?}"
     );
 
+    // Hard-coded response, because the program
+    // is designed to explore the structure of `cURL` HTTP requests,
+    // rather than provide any legitimate responses.
     const RESPONSE: &[u8] = b"HTTP/1.1 405 Method Not Allowed\r\n\
         Allow:\r\n\
         Content-Length: 0\r\n\r\n";
@@ -31,5 +36,7 @@ pub fn main() -> io::Result<()> {
     stream.write(RESPONSE)?;
     stream.flush()?;
 
+    // Waits on `cURL` to exit,
+    // to ensure its output can be viewed.
     child.wait().map(drop)
 }
